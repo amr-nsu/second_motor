a50f8988c4edd3e319ac51cf5124b69ca8bf13d1
amr-nsu <a_maltsev@ngs.ru>
2016-12-05 10:51:04 +0700

    Define to prevent recursive inclusion

--- Inc/func.h
+++ Inc/func.h
@@ -2,6 +2,8 @@
   * @file  func.h
   * @brief Header file of robot control functions
   */
+#ifndef __FUNC_H
+#define __FUNC_H
 
 /**
   * Corrects the PWM value depend on motor
@@ -17,3 +19,5 @@ void ROBOT_Backward(uint16_t);
 void ROBOT_Right(uint16_t);
 void ROBOT_Left(uint16_t);
 void ROBOT_Stop();
+
+#endif  // __FUNC_H
